7d16e3000a6aedcae1215b29f9f66c58c2ad8c58
sutin <injitt>
2023-07-02 03:28:08 +0700

    updated

--- handler/customer.go
+++ handler/customer.go
@@ -2,13 +2,11 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 
-	"github.com/sutin1234/go-hexagonal/errs"
 	"github.com/sutin1234/go-hexagonal/service"
 )
 
@@ -23,8 +21,7 @@ func NewCustomerHandle(cusService service.CustomerService) customerHandle {
 func (h customerHandle) GetCustomers(w http.ResponseWriter, r *http.Request) {
 	customers, err := h.cusService.GetCustomers()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, err)
+		handleError(w, err)
 		return
 	}
 	w.Header().Set("content-type", "application/json")
@@ -34,14 +31,7 @@ func (h customerHandle) GetCustomer(w http.ResponseWriter, r *http.Request) {
 	cusId, _ := strconv.Atoi(mux.Vars(r)["id"])
 	customer, err := h.cusService.GetCustomer(cusId)
 	if err != nil {
-		appErr, ok := err.(errs.AppError)
-		if ok {
-			w.WriteHeader(appErr.Code)
-			fmt.Fprintln(w, appErr.Message)
-			return
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, err)
+		handleError(w, err)
 		return
 	}
 	w.Header().Set("content-type", "application/json")

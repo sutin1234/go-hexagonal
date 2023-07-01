bbf7e5b2307d87ae29ce2288078146e2c6cdbcb3
sutin <injitt>
2023-07-01 19:10:04 +0700

    updated

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/sutin1234/go-hexagonal/repository"
 )
 
 func main() {
+
 	db, err := sqlx.Open("mysql", "root:password@/go_db")
 	if err != nil {
 		panic(err)
 	}
 
 	customerRepo := repository.NewCustomerRepositoryDB(db)
-	_ = customerRepo
+	customers, err := customerRepo.GetAll()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("customers %v", customers)
+
+	customer, err := customerRepo.GetById(1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("customer %v", customer)
 }
--- repository/customer_db.go
+++ repository/customer_db.go
@@ -9,3 +9,22 @@ type customerRepositoryDB struct {
 func NewCustomerRepositoryDB(db *sqlx.DB) customerRepositoryDB {
 	return customerRepositoryDB{db: db}
 }
+
+func (r customerRepositoryDB) GetAll() ([]Customer, error) {
+	customers := []Customer{}
+	query := " select * from customers"
+	err := r.db.Select(&customers, query)
+	if err != nil {
+		return nil, err
+	}
+	return customers, nil
+}
+func (r customerRepositoryDB) GetById(id int) (*Customer, error) {
+	customer := Customer{}
+	query := " select * from customers where customer_id=?"
+	err := r.db.Get(&customer, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return &customer, nil
+}
